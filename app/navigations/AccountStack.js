f8cb67ff68a97b8e262d53e5c4c7529dfccf7ce5
unknown <iinf18.bsantiagot@itesco.edu.mx>
2022-03-25 12:04:17 -0600

    Avanceviernes

---
+++ app/navigations/AccountStack.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Account from '../screens/Account'
+
+const Stack = createStackNavigator()
+
+export default function AccountStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Cuenta'
+                component={Account}
+                options={{tittle:'Mi Cuenta'}}
+            />
+        </Stack.Navigator>
+    )
+}
