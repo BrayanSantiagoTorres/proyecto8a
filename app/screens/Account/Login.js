02469a0e204659c90a95f768dceef0d75d4219ae
unknown <iinf18.bsantiagot@itesco.edu.mx>
2022-04-06 14:02:36 -0500

    login2

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import { StyleSheet, View, ScrollView, Text, Image } from 'react-native'
 import { Divider } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import LoginForm from '../../components/Account/LoginForm'
+import Toast from 'react-native-toast-message'
 
 
 export default function Login(){
+    const toastRef = useRef()
     return(
-        <ScrollView>
+        <KeyboardAwareScrollView>
             <Image
                 source={require('../../../assets/img/logorest.png')}
                 resizeMode='contain'
                 style={styles.logo}
             />
             <View style={styles.viewContainer}>
-                <LoginForm/>
+                <LoginForm toastRef={toastRef}/>
                 <CreateAccount/>
             </View>
             <Divider style={styles.divider}/>
-        </ScrollView>
+            <Toast ref={toastRef}/>
+        </KeyboardAwareScrollView>
     )
 }
 
